056e605abf87102c6f375bd40200ddf84853961f
alexNeto <alex.neto3@hotmail.com>
2017-07-23 20:56:50 -0300

    desisto capitão

--- cameras/migrations/0001_initial.py
+++ cameras/migrations/0001_initial.py
@@ -13,9 +13,26 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Camera',
             fields=[
-                ('id', models.AutoField(auto_created=True, verbose_name='ID', serialize=False, primary_key=True)),
+                ('id', models.AutoField(primary_key=True, auto_created=True, verbose_name='ID', serialize=False)),
                 ('name', models.CharField(max_length=256)),
                 ('address', models.CharField(max_length=2048)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Pergunta',
+            fields=[
+                ('id', models.AutoField(primary_key=True, auto_created=True, verbose_name='ID', serialize=False)),
+                ('idUser', models.IntegerField()),
+                ('pergunta', models.TextField()),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Resposta',
+            fields=[
+                ('id', models.AutoField(primary_key=True, auto_created=True, verbose_name='ID', serialize=False)),
+                ('idPergunta', models.IntegerField()),
+                ('idUser', models.IntegerField()),
+                ('resposta', models.TextField()),
             ],
         ),
     ]
